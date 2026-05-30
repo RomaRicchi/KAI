4e90fbb65d0e8921b17ea192948ef0cbaaa8b30a
labsegroma <lab.seg.roma@gmail.com>
2026-05-30 13:25:35 -0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,12 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
+    id("org.sonarqube") version "6.2.0.5505"
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "labsegroma_KAIZEN")
+        property("sonar.organization", "labsegroma")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }
